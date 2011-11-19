c626607f1c5c38981f444d9b468f0af0d9b8daab
Antti Kervinen <antti.kervinen@intel.com>
2011-11-19 20:32:33 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gdbsearch',
+      version='0.1',
+      description='Search for interesting lines of code',
+      author='Antti Kervinen',
+      author_email='antti.kervinen@gmail.com',
+      url='http://www.github.com/antti.kervinen/gdbsearch',
+      license='GNU General Public License',
+      scripts=['gdbsearch']
+     )
